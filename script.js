875b4ea2b3b22bfc0c7a6c554008ee54b94b92de
umeshchandra24 <120272310+umeshchandra24@users.noreply.github.com>
2023-02-28 11:25:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your code here
+const text = document.getElementById("evaluatedText");
+const count = document.getElementById("count");
+function handleInput(e) {
+	const sentence = e.target.value;
+	const countValue = sentence.split(" ").length;
+	count.textContent = countValue-1;
+}
+text.addEventListener("input", handleInput);
