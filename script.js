11d750a67f7a062eb8c176bf9e56b090ece1d3a8
umeshchandra24 <120272310+umeshchandra24@users.noreply.github.com>
2023-02-28 11:30:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,6 +4,6 @@ const count = document.getElementById("count");
 function handleInput(e) {
 	const sentence = e.target.value;
 	const countValue = sentence.split(" ").length;
-	count.textContent = countValue-1;
+	count.textContent= countValue;
 }
 text.addEventListener("input", handleInput);
